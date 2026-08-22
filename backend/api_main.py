e2b1406e8f5268b5c0fd0642987601509d3aca3b
Mohammed hashir <hashirbearys2024@gmail.com>
2026-08-22 14:42:45 +0530

    api_mainpy md

--- backend/api_main.py
+++ backend/api_main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from backend.api.auth_api import router as auth_router
 from backend.api.books_api import router as books_router
 from backend.api.students_api import router as students_router
@@ -17,6 +17,14 @@ app = FastAPI(
     version="1.0.0"
 )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(books_router)
